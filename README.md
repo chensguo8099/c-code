e36fabfb261fefb81b5916333a64ffa98e9cf545
chensguo8099 <chensguo8099@gmail.com>
2017-03-31 03:05:54 -0700

    2017.3.30

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ## int_char_test.c
 ###### int指针类型的b指向char类型的a时，剩余三个字节为随机数，所以该代码运行结果不是97，是随机值，而且每次随机的不一样（可重入）。
 ###### 可重入的概念：<https://zh.wikipedia.org/wiki/可重入>
+## callbackfunc
+###### 《C和指针》P262-回调函数
---
+++ callback_func/compar_func.c
@@ -0,0 +1,26 @@
+/*
+ * 在一个单链表中查找一个指定值的函数，它的参数是一个指向链表第一节点
+ * 的指针，一个指向我们需要查找的值的指针和一个函数指针，它所指向的函数
+ * 用于比较存储于链表中的类型的值。
+ */
+
+#include <stdio.h>
+#include "node.h"
+
+int compare_ints(void const *a, void const *b){
+	if(*(int *)a == *(int *)b)
+		return 0;
+	else 
+		return 1;
+}
+
+Node*
+search_list(Node *node, void const *value,
+	int (*compare)(void const *, void const *)){
+	while(node != NULL){
+		if(compare(&node->value, value) == 0)
+			break;
+		node = node->link;
+	}
+	return node;
+}
