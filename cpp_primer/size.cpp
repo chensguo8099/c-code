c5f65ac7f689994d6e8f6bb8466645e77840fa3a
chensguo8099 <chensguo8099@gmail.com>
2017-11-01 08:10:26 -0700

    cpp

--- cpp_primer/size.cpp
+++ cpp_primer/size.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
-
+//64位机器测试结果
 int main(){
-	cout << sizeof(bool) << endl;
-	cout << sizeof(char) << endl;
-	cout << sizeof(wchar_t) << endl;
-	cout << sizeof(short) << endl;
-	cout << sizeof(int) << endl;
-	cout << sizeof(long) << endl;
-	cout << sizeof(long long) << endl;
-	cout << sizeof(long double) << endl;
+	cout << sizeof(bool) << endl;//1
+	cout << sizeof(char) << endl;//1
+	cout << sizeof(wchar_t) << endl;//4
+	cout << sizeof(short) << endl;//2
+	cout << sizeof(int) << endl;//4
+	cout << sizeof(long) << endl;//8
+	cout << sizeof(long long) << endl;//8
+	cout << sizeof(long double) << endl;//16
 	return 0;
 }
