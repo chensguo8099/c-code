128e2b262a9a664e76018a03503609f096a179c0
chensguo8099 <chensguo8099@gmail.com>
2019-02-24 00:45:34 +0800

    虚函数的Vptr和虚表

--- cppPrimer/firstStep/test11.cpp
+++ cppPrimer/firstStep/test11.cpp
@@ -9,19 +9,24 @@ using namespace std;
 
 class A{
 public:
+    A(){}
     A(int ax):a(ax){}
     void print(){
         cout<<"a:" << a <<endl;
     }
-private:
+    void print2(){
+        cout << "a" << a << endl;
+    }
     int a;
+private:
 };
 
 class B : virtual public A{
 public:
     B(int ax,int bx):A(ax),b(bx){}
+    //在构造B时会先构造A，所以一开始B的VPTR指针指向A类的虚表，当构造A完毕后VPTR再由A类指向B类！！
     void print(){
-        cout<<"a:" << b <<endl;
+        cout<<"b:" << b <<endl;
     }
 private:
     int b;
@@ -31,7 +36,7 @@ class C : virtual public A{
 public:
     C(int ax,int cx):A(ax),c(cx){}
     void print(){
-        cout<<"a:" << c <<endl;
+        cout<<"c:" << c <<endl;
     }
 private:
     int c;
@@ -40,9 +45,11 @@ private:
 class D : public B, public C 
 {
 public:
-    D(int ax,int bx,int cx, int dx):A(ax), B(ax,bx), C(ax,cx),d(dx){}
+    D(int ax,int bx,int cx, int dx):A(ax), B(ax,bx), C(ax,cx),d(dx){
+        cout << this->B::a << endl;
+    }
     void print(){
-        cout<<"a:" << d <<endl;
+        cout<<"d:" << d <<endl;
     }
 private:
     int d;
@@ -58,6 +65,14 @@ int main(int argc, char const *argv[])
     //A *pa = (B*) &d;  //上行转换出现二义性,d中含有两个基类对象A，隐式转换不知道让pa指向哪个子对象
     //pa->print(); //print具有二义性，系统不知道是调用B类还是C类的print()
     /*virtaul继承 要注意修改D的构造函数，必须加上A的参数构造，否则默认调用A的无参构造*/
-    d.print();
+    d.print();//打印d:4
+    A *ap = new D(6, 7, 8, 9);
+    ap->print();//A类中的print不加virtual打印a:6
+    //A类中print加virtual后为d:9
+    A *ap2 = new D(5, 0, 0, 100);
+    ap2->print2();//打印a0 
+    ap2->print();//打印d:100
+    A *ap3 = new B(50, 500);
+    ap3->print();
     return 0;
 }
